1267e7cd6878098cb4c9ade25774e521d092f29d
Emanuel Jose <emanuel.jose@autoglass.com.br>
2023-11-25 19:59:44 -0300

    Implementa método para leitura de números float

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Calculadora
+﻿using System;
+
+namespace Calculadora
 {
     class Program
     {
         static void Main(string[] args)
         {
 
+        }
+
+        static float LeFloat(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+
+            float numero = 0;
+            string? leitura = Console.ReadLine();
+
+            if (float.TryParse(leitura, out numero) == false)
+            {
+                Console.WriteLine("AVISO: O número não foi lido corretamente, considerando como 0.");
+            }
+
+            return numero;
         }
     }
 }
